dd90265e6253a1a471466960e8de29d8420f167f
hackzhu <3110497917@qq.com>
2022-08-23 11:21:18 +0800

    add mqtt callback

---
+++ mcallback.py
@@ -0,0 +1,35 @@
+from sqlite3 import connect
+import paho.mqtt.client as mqtt
+
+mhost = "hackzhu.com"
+mport = 1883
+mqttc = mqtt.Client()
+
+
+def mcon():
+    mqttc.connect(mhost, mport, 60)
+    mqttc.loop_start()
+
+
+def mmsg(client, userdata, msg):
+    mpayload = str(msg.payload)[2:-1]
+    if mpayload == '1':
+        print('this is 1')
+    elif mpayload=='2':
+        print('this is 2')
+
+
+def msub():
+    mqttc.subscribe("hass/script")
+    mqttc.on_message = mmsg
+
+
+def main():
+    mcon()
+    msub()
+    while 1:
+        pass
+
+
+if __name__ == '__main__':
+    main()
