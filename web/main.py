9fbcf20459869b5978357b0405b6910d4cc92f16
hackzhu <3110497917@qq.com>
2022-10-04 11:28:47 +0800

    flask upload

--- web/main.py
+++ web/main.py
@@ -4,17 +4,30 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'web/upload/'
+app.config['UPLOAD_FOLDER'] = 'tmp/upload'
+app.config['TMP_FOLDER'] = 'tmp'
 
+if not os.path.exists('tmp/upload'):
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    os.mkdir('tmp/upload')
+if not os.path.exists('tmp/userhost'):
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    os.system('touch tmp/userhost')
 
-if not os.path.exists('web/upload'):
-    os.mkdir('web/upload')
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    with open('tmp/userhost') as hf:
+        user_host = hf.read().splitlines()
+    context = {
+        "userhost": user_host
+    }
+    return render_template('index.html', **context)
 
 
+# BUG 中文文件名
 @app.route('/uploader', methods=['GET', 'POST'])
 def uploader():
     if request.method == 'POST':
@@ -22,12 +35,19 @@ def uploader():
         print(request.files)
         f.save(os.path.join(
             app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-
-        return 'file uploaded successfully'
-
+        return 'file uploaded successlly'
     else:
-
         return render_template('index.html')
+
+
+@app.route('/userhost', methods=['GET', 'POST'])
+def hostname():
+    if request.method == 'POST':
+        hosts = request.form['host']
+        # for h in hosts:
+
+        return 'host file uploaded successlly'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
