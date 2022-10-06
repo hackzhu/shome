a53697e01717fd2c724ea32559d96bb6f3bd1685
hackzhu <3110497917@qq.com>
2022-10-06 13:15:25 +0800

    add ip check

--- web/main.py
+++ web/main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
 import os
+import IPy
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'tmp/upload'
 app.config['TMP_FOLDER'] = 'tmp'
 
+## 创建所需目录
 if not os.path.exists('tmp/upload'):
     if not os.path.exists('tmp'):
         os.mkdir('tmp')
@@ -15,6 +17,17 @@ if not os.path.exists('tmp/userhost'):
     if not os.path.exists('tmp'):
         os.mkdir('tmp')
     os.system('touch tmp/userhost')
+
+## 检测ip合法性
+def checkip(address):
+    try:
+        version = IPy.IP(address).version()
+        if version == 4 or version == 6:
+            return True
+        else:
+            return False
+    except Exception as e:
+        return False
 
 
 @app.route('/')
@@ -41,12 +54,14 @@ def uploader():
 
 
 @app.route('/userhost', methods=['GET', 'POST'])
-def hostname():
+def hostip():
     if request.method == 'POST':
-        hosts = request.form['host']
-        # for h in hosts:
-
-        return 'host file uploaded successlly'
+        hosts = request.form['host'].splitlines()
+        userhost = []
+        for h in hosts:
+            if checkip(h) == True:
+                userhost.append(h)
+        return userhost
 
 
 if __name__ == '__main__':
