b105b9b7fbba1957269d629a9b7c464b3f491039
hackzhu <3110497917@qq.com>
2022-11-03 00:05:10 +0800

    run cv correctly

--- web/main.py
+++ web/main.py
@@ -1,28 +1,28 @@
-from flask import Flask, render_template, request, Response, make_response
+from flask import Flask, render_template, request, Response
 from werkzeug.utils import secure_filename
-from camera import VideoCamera
+# from camera import VideoCamera
 from pypinyin import lazy_pinyin
 
 import os
 import IPy
+import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'tmp/upload'
 app.config['TMP_FOLDER'] = 'tmp'
 
 # 创建所需目录
-if not os.path.exists('tmp/upload'):
-    if not os.path.exists('tmp'):
-        os.mkdir('tmp')
-    os.mkdir('tmp/upload')
-if not os.path.exists('tmp/userhost'):
-    if not os.path.exists('tmp'):
-        os.mkdir('tmp')
-    os.system('touch tmp/userhost')
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    if not os.path.exists(app.config['TMP_FOLDER']):
+        os.mkdir(app.config['TMP_FOLDER'])
+    os.mkdir(app.config['UPLOAD_FOLDER'])
+if not os.path.exists(app.config['TMP_FOLDER']+'/userhost'):
+    if not os.path.exists(app.config['TMP_FOLDER']):
+        os.mkdir(app.config['TMP_FOLDER'])
+    os.system('touch ' + app.config['TMP_FOLDER'] + '/userhost')
+
 
 # 检测ip合法性
-
-
 def checkip(address):
     try:
         version = IPy.IP(address).version()
@@ -34,54 +34,58 @@ def checkip(address):
         return False
 
 
-# video
-def video_push(camera):
+# 视频推流
+def video_push():
+    camera = cv2.VideoCapture(0)
     while True:
-        frame = camera.get_frame()
+        success, frame = camera.read()  # read the camera frame
+        detector = cv2.CascadeClassifier(
+            'opencv/haarcascade_frontalface_default.xml')
+        faces = detector.detectMultiScale(frame, 1.2, 6)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-@app.route('/video_pull')
-def video_pull():
-    return Response(video_push(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# video
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/')
 def index():
-    with open('tmp/userhost') as hf:
+    with open(app.config['TMP_FOLDER']+'/userhost') as hf:
         user_host = hf.read().splitlines()
     context = {
         "userhost": user_host
     }
     return render_template('index.html', **context)
 
-# 中文转拼音上传
-@app.route('/uploader', methods=['GET', 'POST'])
+
+@app.route('/video_pull')
+def video_pull():
+    return Response(video_push(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+#! 中文转拼音上传
+@app.route('/uploader', methods=['POST'])
 def uploader():
-    if request.method == 'POST':
-        f = request.files['file']
-        print(request.files)
-        f.save(os.path.join(
-            app.config['UPLOAD_FOLDER'], secure_filename(''.join(lazy_pinyin(f.filename)))))
-        return 'file uploaded successlly'
-    else:
-        return render_template('index.html')
+    f = request.files['file']
+    print(request.files)
+    f.save(os.path.join(
+        app.config['UPLOAD_FOLDER'], secure_filename(''.join(lazy_pinyin(f.filename)))))
+    return '上传成功'
 
 
-@app.route('/userhost', methods=['GET', 'POST'])
+@app.route('/userhost', methods=['POST'])
 def hostip():
-    if request.method == 'POST':
-        hosts = request.form['host'].splitlines()
-        userhost = []
-        for h in hosts:
-            if checkip(h) == True:
-                userhost.append(h)
-        return userhost
+    hosts = request.form['host'].splitlines()
+    userhost = []
+    for h in hosts:
+        if checkip(h) == True:
+            userhost.append(h)
+    return userhost
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000)
