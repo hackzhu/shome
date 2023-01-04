c43945118f29876d879a49ee77c89aa692f81320
hackzhu <3110497917@qq.com>
2023-01-04 11:53:54 +0800

    login

--- shome/main.py
+++ shome/main.py
@@ -5,8 +5,13 @@ import cv2
 import init
 import json
 
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, redirect, url_for
 from flask_mqtt import Mqtt
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from pypinyin import lazy_pinyin
 # from xiaoai import *  # 小爱同学不支持IPv6
@@ -25,8 +30,25 @@ try:
     mqttclient = Mqtt(app)
 except:
     pass
+app.config['SECRET_KEY'] = 'abc'  # 设置表单交互密钥
+login_manager = LoginManager()  # 实例化登录管理对象
+login_manager.init_app(app)  # 初始化应用
+login_manager.login_view = 'login'  # 设置用户登录视图函数 endpoint
 cvfont = cv2.FONT_HERSHEY_SIMPLEX
 config = init.config_read()
+
+USERS = [
+    {
+        "id": 1,
+        "name": 'li',
+        "password": generate_password_hash('123')
+    },
+    {
+        "id": 2,
+        "name": 'tom',
+        "password": generate_password_hash('123')
+    }
+]
 
 try:
     @mqttclient.on_connect()
@@ -40,7 +62,6 @@ try:
                 print('Bad connection. Code:', rc)
         except:
             print('Bad connection. Code:', rc)
-
 
     @mqttclient.on_message()
     def mqtt_callback(client, userdata, message) -> None:
@@ -77,6 +98,54 @@ try:
             return None
 except NameError:
     pass
+
+
+class LoginForm(FlaskForm):
+    """登录表单类"""
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+
+
+class User(UserMixin):
+    """用户类"""
+    def __init__(self, user):
+        self.username = user.get("name")
+        self.password_hash = user.get("password")
+        self.id = user.get("id")
+
+    def verify_password(self, password):
+        """密码验证"""
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        """获取用户ID"""
+        return self.id
+
+    @staticmethod
+    def get(user_id):
+        """根据用户ID获取用户实体，为 login_user 方法提供支持"""
+        if not user_id:
+            return None
+        for user in USERS:
+            if user.get('id') == user_id:
+                return User(user)
+        return None
+
+
+def get_user(user_name):
+    """根据用户名获得用户记录"""
+    for user in USERS:
+        if user.get("name") == user_name:
+            return user
+    return None
+
+
+@login_manager.user_loader  # 定义获取登录用户的方法
+def load_user(user_id):
+    return User.get(user_id)
+
 
 # 视频推流
 def video_push():
@@ -124,14 +193,43 @@ def xiaoai_input(event):
 
 
 @app.route('/')
+@login_required  # 需要登录才能访问
 def index():
     context = {
         'userip': config.get('userip'),
         'athome': config.get('athome'),
         'ddnsip': config.get('ddnsip'),
-        'device': config.get('device')
+        'device': config.get('device'),
+        'username': current_user.username
     }
     return render_template('index.html', **context)
+
+
+@app.route('/login', methods=('GET', 'POST'))  # 登录
+def login():
+    form = LoginForm()
+    emsg = None
+    if form.validate_on_submit():
+        user_name = form.username.data
+        password = form.password.data
+        user_info = get_user(user_name)  # 从用户数据中查找用户记录
+        if user_info is None:
+            emsg = "用户名或密码密码有误"
+        else:
+            user = User(user_info)  # 创建用户实体
+            if user.verify_password(password):  # 校验密码
+                login_user(user)  # 创建用户 Session
+                return redirect(request.args.get('next') or url_for('index'))
+            else:
+                emsg = "用户名或密码密码有误"
+    return render_template('login.html', form=form, emsg=emsg)
+
+
+@app.route('/logout')  # 登出
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 '''
@@ -143,6 +241,7 @@ def xiaoai():
 
 
 @app.route('/userip_update', methods=['POST', 'GET'])
+@login_required
 def user_ip():
     try:
         global config
@@ -164,6 +263,7 @@ def user_ip():
 
 # TODO 自动获取ip
 @app.route('/ddns', methods=['POST', 'GET'])
+@login_required
 def ddns():
     try:
         global config
@@ -189,6 +289,7 @@ def ddns():
 # 后续可能从secure_filename源码处修改
 # opencv putText的中文问题该解决
 @app.route('/face_upload', methods=['POST', 'GET'])
+@login_required
 def face_upload():
     try:
         if request.method == 'POST':
@@ -204,6 +305,7 @@ def face_upload():
 
 
 @app.route('/device_update', methods=['POST', 'GET'])
+@login_required
 def device_update():
     try:
         global config
@@ -239,6 +341,7 @@ def device_update():
 
 
 @app.route('/video_pull')
+@login_required
 def video_pull():
     return Response(video_push(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
